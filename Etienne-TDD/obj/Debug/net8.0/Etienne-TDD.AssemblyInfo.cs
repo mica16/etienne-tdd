15dc61b01c29858106c017d8385258a85c8c8220
Michaël AZERHAD <mazerhad@wealcomecompany.com>
2024-10-28 10:25:20 +0100

    start project

--- Etienne-TDD/obj/Debug/net8.0/Etienne-TDD.AssemblyInfo.cs
+++ Etienne-TDD/obj/Debug/net8.0/Etienne-TDD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Etienne-TDD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a896bfd5e2e1f90f4b254e8eadb0acef00db6da0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Etienne-TDD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Etienne-TDD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
